05665c48eb1f1e44be99aff12f8bffb34f7f1ee7
Anaïs <c7anais@gmail.com>
2021-10-30 18:32:04 +0200

    add wireframes and functional tree diagram

--- js/main.js
+++ js/main.js
@@ -122,11 +122,12 @@ fetch('https://oc-jswebsrv.herokuapp.com/api/articles')
             let blog = document.getElementById("blog");
                 for (let i=0; i<response.length; i++) {
                     blog.innerHTML += `<article class="card col-10 col-sm-7 col-md-5 col-lg-4 col-xl-3 mb-5 mt-md-5 mx-2 mx-md-4 p-0">
-                                        <h5 class="card-header bg-Kobi text-white">${response[i].titre} n°${response[i].id}</h5>
-                                        <p class="card-body">${response[i].contenu}</p>
-                                        <div class="d-flex justify-content-center pb-2">
-                                        <button class="btn btn-transaction m-1">See</button>
-                                        </div></article>`;
+                                            <h5 class="card-header bg-Kobi text-white">${response[i].titre} n°${response[i].id}</h5>
+                                            <p class="card-body">${response[i].contenu}</p>
+                                            <div class="d-flex justify-content-center pb-2">
+                                                <button class="btn btn-transaction m-1">See</button>
+                                            </div>
+                                        </article>`;
                 }
 
         })
